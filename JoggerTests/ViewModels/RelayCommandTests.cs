b0812d402fe6a74952c6a40d577032310e207f9b
MSobczak <m.sobcz@gmail.com>
2020-05-04 00:53:31 +0200

    RelayCommandWithTests

---
+++ JoggerTests/ViewModels/RelayCommandTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jogger.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jogger.ViewModels.Tests
+{
+    [TestClass()]
+    public class RelayCommandTests
+    {
+        [TestMethod()]
+        public void SingleActionExecution()
+        {
+            var executionCounter = 0;
+            RelayCommand relayCommand = new RelayCommand((o) => executionCounter++, o => true);
+            relayCommand.Execute(new object());
+            Assert.AreEqual(1, executionCounter);
+        }
+        [TestMethod()]
+        public void DoubleActionExecution()
+        {
+            var executionCounter = 0;
+            RelayCommand relayCommand = new RelayCommand((o) => executionCounter++, o => true);
+            relayCommand.Execute(new object());
+            relayCommand.Execute(new object());
+            Assert.AreEqual(2, executionCounter);
+        }
+        [TestMethod()]
+        public void CanNotExecuteWithTypedFalse()
+        {
+            var executionCounter = 0;
+            RelayCommand relayCommand = new RelayCommand((o) => executionCounter++, o => false);
+            Assert.AreEqual(false, relayCommand.CanExecute(new object()));
+        }
+        [TestMethod()]
+        public void CanExecuteWithDefaultCanExecute()
+        {
+            var executionCounter = 0;
+            RelayCommand relayCommand = new RelayCommand((o) => executionCounter++);
+            Assert.AreEqual(true, relayCommand.CanExecute(new object()));
+        }
+    }
+}
